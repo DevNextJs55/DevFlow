7f117ed659613602fa63376ae7d9abee649512e1
Dev Js <bhuv059@gmail.com>
2024-04-28 19:10:03 +0200

    add_assets_layout_modify

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,29 @@
+/* eslint-disable camelcase */
 import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Devflow",
   description: "QnA app for developers",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export default function RootLayout({
   children,
@@ -14,9 +31,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is the heading</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
